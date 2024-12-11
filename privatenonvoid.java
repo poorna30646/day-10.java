0e087e66c427c78411f92f1ee13430c48fcaad34
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 10:50:46 +0530

    Add files via upload

---
+++ privatenonvoid.java
@@ -0,0 +1,11 @@
+class main{//Static non void method
+    private static String s()
+    {
+        return "hello";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
