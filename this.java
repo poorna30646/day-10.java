bf2bc445c7bc065e9a8d26604960ef70270ca4af
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 14:40:08 +0530

    Add files via upload

---
+++ this.java
@@ -0,0 +1,15 @@
+class Main{
+    void display()
+    {
+        System.out.println("Hi poorna");
+    }
+    void dis1()
+    {
+        this.display();
+        }
+        public static void main(String args[])
+        {
+            Main o=new Main();
+            o.dis1();
+        }
+}
