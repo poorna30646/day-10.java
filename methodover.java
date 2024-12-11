e58f4a538863a39135019f816059f60e20c60f07
poorna30646 <thotipoornachandra@gmail.com>
2024-12-11 10:48:59 +0530

    Add files via upload

---
+++ methodover.java
@@ -0,0 +1,20 @@
+class main{//METHOD OVER LOADING
+    static void a()
+    {
+        System.out.println("hello");
+    }
+    static void a(int b)
+    {
+        System.out.println("hello "+b);
+    }
+    static void a(int b,int c)
+    {
+        System.out.println("hello "+b+" "+c);
+    }
+    public static void main(String args[])
+    {
+        a();
+        a(6);
+        a(4,5);
+    }
+}
